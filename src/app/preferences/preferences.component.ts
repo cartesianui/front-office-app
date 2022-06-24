c254ed56dc4312324b0c62ec196851ed0c11fde1
Osama Khan <osama@orenda.systems>
2022-06-24 15:15:50 +0500

    Added Preferences UI with Sample Data

--- src/app/preferences/preferences.component.ts
+++ src/app/preferences/preferences.component.ts
@@ -1,11 +1,44 @@
 import { Component, Injector } from '@angular/core';
 import { BaseComponent } from '@cartesianui/common';
 
+type ThemeType = 'dark' | 'light' | 'sys';
+type ColorSchemeType = 'blue' | 'purple' | 'green' | 'red';
+
 @Component({
-  templateUrl: 'preferences.component.html'
+  templateUrl: 'preferences.component.html',
+  styleUrls: ['preferences.component.scss']
 })
 export class PreferencesComponent extends BaseComponent {
+  interface = {
+    theme: 'light',
+    colorScheme: 'blue'
+  };
+  notifications = {
+    enabled: true,
+    chat: true,
+    promotions: false,
+    updates: true
+  };
+
+  themes = [
+    { name: 'Light', value: 'light' },
+    { name: 'Dark', value: 'dark' },
+    { name: 'System', value: 'sys' }
+  ];
+
+  colorSchemes = [
+    { name: 'blue', color: '#007aff' },
+    { name: 'purple', color: '#7a00ff' },
+    { name: 'green', color: '#00ff7a' },
+    { name: 'red', color: '#ff007a' }
+  ];
+
   constructor(injector: Injector) {
     super(injector);
   }
+
+  get themeName() {
+    return this.themes.find((t) => t.value === this.interface.theme).name;
+  }
+  setTheme = (theme: ThemeType) => (this.interface.theme = theme);
 }
--- src/app/preferences/preferences.module.ts
+++ src/app/preferences/preferences.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 
 import { PreferencesComponent } from './preferences.component';
 import { PreferencesRoutingModule } from './preferences-routing.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  imports: [PreferencesRoutingModule],
+  imports: [PreferencesRoutingModule, CommonModule],
   declarations: [PreferencesComponent]
 })
 export class PreferencesModule {}
